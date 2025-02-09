7a634e5ae6bfaf0f79d65276e6a566782a867d61
kirisu25 <kirisu25@gmail.com>
2025-02-10 01:11:51 +0900

    update:lsp-config

--- config/luanix/nvim-lspconfig.lua.nix
+++ config/luanix/nvim-lspconfig.lua.nix
@@ -1,9 +1,9 @@
 { pkgs }:
 ''
   local nvim_lsp = require("lspconfig")
-  nvim_lsp.tsserver.setup({
+  nvim_lsp.ts_ls.setup({
     init_options = {
-      tsserver = {
+      ts_ls = {
         path = "${pkgs.nodePackages.typescript}/lib/node_modules/typescript/lib",
       },
     },
