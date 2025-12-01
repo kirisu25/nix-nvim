d35c7d06f8b280e6c8d1f76d49addac4cbb7945c
kirisu25 <kirisu25@gmail.com>
2025-12-01 14:34:02 +0900

    new

--- flake.nix
+++ flake.nix
@@ -1,15 +1,28 @@
 {
-  description = "A very basic flake";
+  description = "my neovim";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # utils.url = "github:numtide/flake-utils";
+    neovim = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      # utils,
+      neovim,
+      ...
+    }:
+    {
+      packages.x86_64-linux.default = neovim.packages.x86_64-linux.neovim;
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
+      };
+    };
 }
